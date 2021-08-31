efc20cb574a4359e8ba18f0690d3f18921968a20
Blucilhã <blucilha.nascimento@gmail.com>
2021-08-31 13:04:21 -0300

    Criação da calculadora

--- src/br/Principal/Principal.java
+++ src/br/Principal/Principal.java
@@ -1,13 +1,30 @@
 package br.Principal;
 
-import br.com.Animal.Animal;
+import java.util.Scanner;
+
+import br.com.Calculadora.Calculadora;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		Animal animal = new Animal("felino", "Tigre", 4, true, true, false, false, true, false);
-		System.out.println(animal.toString());
-
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro inteiro :");
+		int a = scan.nextInt();
+		System.out.println("Digite o segundo inteiro :");
+		int b = scan.nextInt();
+		
+		Calculadora calculadora = new Calculadora();
+		
+		int soma = calculadora.Soma(a, b);
+		int subtracao = calculadora.Subtracao(a, b);
+		int multiplicacao = calculadora.multiplicacao(a, b);
+		int divisao = calculadora.divisao(a, b);
+		
+		System.out.println("sum: " + soma);
+		System.out.println("sub: " + subtracao);
+		System.out.println("mult: " + multiplicacao);
+		System.out.println("div: " + divisao);
 	}
 
 }
---
+++ src/br/com/Calculadora/Calculadora.java
@@ -0,0 +1,20 @@
+package br.com.Calculadora;
+
+public class Calculadora {
+	public int Soma(int a, int b) {
+		return a + b;
+	}
+	
+	public int Subtracao(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiplicacao(int a, int b) {
+		return a * b;
+	}
+	
+	public int divisao(int a, int b) {
+		return a / b;
+	}
+	
+}
